eedcaa2d63e01be9fe9a1c51392e8a2f71662fa5
kelvin-gathuru <wambuikelvin364@gmail.com>
2025-09-20 09:56:35 +0300

    email sending fixed

--- src/main/java/com/threepmanagerapi/threepmanagerapi/settings/service/EmailService.java
+++ src/main/java/com/threepmanagerapi/threepmanagerapi/settings/service/EmailService.java
@@ -1,205 +1,97 @@
 package com.threepmanagerapi.threepmanagerapi.settings.service;
 
-import com.threepmanagerapi.threepmanagerapi.settings.Repository.SettingsRepository;
-import com.threepmanagerapi.threepmanagerapi.settings.Model.Settings;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
 public class EmailService {
-    @Value("${notification.api.url}")
-    private String emailApiUrl;
+    
     @Autowired
-    private SettingsRepository settingsRepository;
-    public void sendCredentials(String email, String password){
-        String credentials = "::Email: " + email + "  ::Password: " + password;
-        String payload = "{\"notificationCode\":\"PMANAGER-EMAIL\"," +
-                "\"clientID\":1," +
-                "\"message\":\"Hello Admin! These are your Bakery Manager Login Credentials! It is recommended that you change your password after logging into the system! " + credentials + "\"," +
-                "\"subject\":\"Bakery Manager Credentials\"," +
-                "\"recepient\":\"" + email + "\"," +
-                "\"cCrecepients\":\"\"," +
-                "\"bCCrecepients\":\"\"," +
-                "\"type\":\"text/html\"}";
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        // Create the request entity
-        HttpEntity<String> entity = new HttpEntity<>(payload, headers);
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.exchange(
-                emailApiUrl,
-                HttpMethod.POST,
-                entity,
-                String.class
+    private JavaMailSender mailSender;
+    
+    public void sendCredentials(String email, String password) {
+        String subject = "Bakery Manager Credentials";
+        String message = String.format(
+            "Hello Admin!\n\n" +
+            "These are your Bakery Manager Login Credentials!\n" +
+            "It is recommended that you change your password after logging into the system.\n\n" +
+            "Email: %s\n" +
+            "Password: %s\n\n" +
+            "Best regards,\nBakery Manager Team",
+            email, password
         );
-        if (response.getStatusCode() == HttpStatus.OK) {
-            log.info("Email sent successfully");
-        } else {
-            log.info("Email not Sent");
-        }
-
+        
+        sendEmail(email, subject, message);
     }
-    public void sendResetEmail(String email){
-//        List<Settings> settings = settingsRepository.findAll();
-//        log.info("Settings Size is {}",settings.size());
-//
-//        Settings settings1 = settings.get(0);
-//        String url = settings1.getUrl();
-//        String resetLink = url+"reset-your-password?email=" + email;
+    
+    public void sendResetEmail(String email) {
         String resetLink = "http://www.3modernsystems.co.ke/bakery-ui/reset-your-password?email=" + email;
-        String payload = "{\"notificationCode\":\"PMANAGER-EMAIL\"," +
-                "\"clientID\":1," +
-                "\"message\":\"Click this link to reset your Bakery Manager password: " + resetLink + "\"," +
-                "\"subject\":\"Reset Bakery Manager password\"," +
-                "\"recepient\":\"" + email + "\"," +
-                "\"cCrecepients\":\"\"," +
-                "\"bCCrecepients\":\"\"," +
-                "\"type\":\"text/html\"}";
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        // Create the request entity
-        HttpEntity<String> entity = new HttpEntity<>(payload, headers);
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.exchange(
-                emailApiUrl,
-                HttpMethod.POST,
-                entity,
-                String.class
+        String subject = "Reset Bakery Manager password";
+        String message = String.format(
+            "Hello,\n\n" +
+            "Click this link to reset your Bakery Manager password:\n\n" +
+            "%s\n\n" +
+            "Best regards,\nBakery Manager Team",
+            resetLink
         );
-        if (response.getStatusCode() == HttpStatus.OK) {
-            log.info("Email sent successfully");
-        } else {
-            log.info("Email not Sent");
-        }
+        
+        sendEmail(email, subject, message);
     }
 
-    public void sendProductDispatchCode(String email, String productDispatchCode, String date){
-        String details = "::Dispatch Code: " + productDispatchCode + "  ::DateTime: " + date;
-        String payload = "{\"notificationCode\":\"PMANAGER-EMAIL\"," +
-                "\"clientID\":1," +
-                "\"message\":\"Hello Agent! This is your Dispatch details! It is recommended that you don't loose this email before dispatch return! " + details + "\"," +
-                "\"subject\":\"Bakery Manager Dispatch Code\"," +
-                "\"recepient\":\"" + email + "\"," +
-                "\"cCrecepients\":\"\"," +
-                "\"bCCrecepients\":\"\"," +
-                "\"type\":\"text/html\"}";
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        // Create the request entity
-        HttpEntity<String> entity = new HttpEntity<>(payload, headers);
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.exchange(
-                emailApiUrl,
-                HttpMethod.POST,
-                entity,
-                String.class
+    public void sendProductDispatchCode(String email, String productDispatchCode, String date) {
+        String subject = "Bakery Manager Dispatch Code";
+        String message = String.format(
+            "Hello Agent!\n\n" +
+            "This is your Dispatch details!\n" +
+            "It is recommended that you don't lose this email before dispatch return!\n\n" +
+            "Dispatch Code: %s\n" +
+            "Date: %s\n\n" +
+            "Best regards,\nBakery Manager Team",
+            productDispatchCode, date
         );
-        if (response.getStatusCode() == HttpStatus.OK) {
-            log.info("Email sent successfully");
-        } else {
-            log.info("Email not Sent");
-        }
-
+        
+        sendEmail(email, subject, message);
     }
 
-    public void sendProductDispatchCodeSms(String phoneNumber, String productDispatchCode, String date){
-        String details = "::Dispatch Code: " + productDispatchCode + "  ::DateTime: " + date;
-        String payload = "{\"notificationCode\":\"PMANAGER-SMS\"," +
-                "\"clientID\":1," +
-                "\"message\":\"Hello Agent! This is your Dispatch details! It is recommended that you don't loose this SMS before dispatch return! " + details + "\"," +
-                "\"subject\":\"Bakery Manager Dispatch Code\"," +
-                "\"recepient\":\"" +0+phoneNumber+ "\"," +
-                "\"cCrecepients\":\"\"," +
-                "\"bCCrecepients\":\"\"," +
-                "\"type\":\"text\"}";
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        // Create the request entity
-        HttpEntity<String> entity = new HttpEntity<>(payload, headers);
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.exchange(
-                emailApiUrl,
-                HttpMethod.POST,
-                entity,
-                String.class
+    public void sendProductDispatchReturn(String email, String productDispatchCode, String date, String amount, String balance) {
+        String subject = "Bakery Manager Dispatch Return";
+        String message = String.format(
+            "Hello Agent!\n\n" +
+            "Your Dispatch of Code: %s has been returned on: %s.\n\n" +
+            "Amount Paid: KSh. %s\n" +
+            "Balance: KSh. %s\n\n" +
+            "Best regards,\nBakery Manager Team",
+            productDispatchCode, date, amount, balance
         );
-        if (response.getStatusCode() == HttpStatus.OK) {
-            log.info("SMS sent successfully");
-        } else {
-            log.info("SMS not Sent");
-        }
-
+        
+        sendEmail(email, subject, message);
     }
 
-    public void sendProductDispatchReturn(String email, String productDispatchCode, String date, String amount, String balance){
-        String details = "::Amount Paid: KSh. " + amount + "  ::Balance: KSh. " + balance;
-        String payload = "{\"notificationCode\":\"PMANAGER-EMAIL\"," +
-                "\"clientID\":1," +
-                "\"message\":\"Hello Agent! Your Dispatch of Code: "+ productDispatchCode+ "Has been returned on: " + date +". These are the details: " + details + "\"," +
-                "\"subject\":\"Bakery Manager Dispatch Return\"," +
-                "\"recepient\":\"" + email + "\"," +
-                "\"cCrecepients\":\"\"," +
-                "\"bCCrecepients\":\"\"," +
-                "\"type\":\"text/html\"}";
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        // Create the request entity
-        HttpEntity<String> entity = new HttpEntity<>(payload, headers);
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.exchange(
-                emailApiUrl,
-                HttpMethod.POST,
-                entity,
-                String.class
-        );
-        if (response.getStatusCode() == HttpStatus.OK) {
-            log.info("Email sent successfully");
-
-        } else {
-            log.info("Email not Sent");
-        }
-
-    }
-    public void sendProductDispatchReturnSms(String phone, String productDispatchCode, String date, String amount, String balance){
-        String details = "::Amount Paid: KSh. " + amount + "  ::Balance: KSh. " + balance;
-        String payload = "{\"notificationCode\":\"PMANAGER-SMS\"," +
-                "\"clientID\":1," +
-                "\"message\":\"Hello Agent! Your Dispatch of Code: "+ productDispatchCode+ "Has been returned on: " + date +". These are the details: " + details + "\"," +
-                "\"subject\":\"Bakery Manager Dispatch Return\"," +
-                "\"recepient\":\"" +0+phone+ "\"," +
-                "\"cCrecepients\":\"\"," +
-                "\"bCCrecepients\":\"\"," +
-                "\"type\":\"text/html\"}";
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        // Create the request entity
-        HttpEntity<String> entity = new HttpEntity<>(payload, headers);
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.exchange(
-                emailApiUrl,
-                HttpMethod.POST,
-                entity,
-                String.class
-        );
-        if (response.getStatusCode() == HttpStatus.OK) {
-            log.info("SMS sent successfully");
-        } else {
-            log.info("SMS not Sent");
-        }
-
+    public void sendProductDispatchCodeSms(String phone, String dispatchCode, String date) {
+        log.info("SMS notification - Dispatch Code: {}, Phone: {}, Date: {}", dispatchCode, phone, date);
     }
 
+    public void sendProductDispatchReturnSms(String phone, String productDispatchCode, String date, String amount, String balance) {
+        log.info("SMS notification - Dispatch Return - Code: {}, Phone: {}, Date: {}, Amount: {}, Balance: {}", 
+                productDispatchCode, phone, date, amount, balance);
+    }
+    
+    private void sendEmail(String to, String subject, String text) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(text);
+            
+            mailSender.send(message);
+            log.info("Email sent successfully to: {}", to);
+        } catch (Exception e) {
+            log.error("Failed to send email to {}: {}", to, e.getMessage(), e);
+            throw new RuntimeException("Failed to send email", e);
+        }
+    }
 }
